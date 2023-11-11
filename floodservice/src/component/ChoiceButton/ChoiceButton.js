5531fc8c21a75f8c75b9d699f05014a21178dc51
Danvace <vipdan055@gmail.com>
2023-11-11 15:08:29 +0200

    change logic when adding card in catalog

--- floodservice/src/component/ChoiceButton/ChoiceButton.js
+++ floodservice/src/component/ChoiceButton/ChoiceButton.js
@@ -1,37 +1,170 @@
+// import React from 'react';
+// import {ButtonContainer, ChoiceButton, StyledSelect} from "./ChoiceButton.styled";
+//
+//
+// const filters = [{
+//     title: "Price",
+//     options: ["ascending", "descending"]
+// },
+//     {
+//         title: "Year",
+//         options: ["ascending", "descending"]
+//     },
+//     {
+//         title: "Engine volume",
+//         options: ["ascending", "descending"]
+//     }]
+
+// function ThreeChoiceButton() {
+//     const [selectedChoice, setSelectedChoice] = useState(null);
+//
+//     const handleChoiceClick = (choice) => {
+//         setSelectedChoice(selectedChoice === choice ? null : choice);
+//     };
+
+// return (
+//     <ButtonContainer>
+//         <ChoiceButton
+//             selected={selectedChoice === 'choice1'}
+//             onClick={() => handleChoiceClick('choice1')}
+//         >
+//             Price
+//         </ChoiceButton>
+//
+//         <ChoiceButton
+//             selected={selectedChoice === 'choice2'}
+//             onClick={() => handleChoiceClick('choice2')}
+//         >
+//             Year
+//         </ChoiceButton>
+//
+//         <ChoiceButton
+//             selected={selectedChoice === 'choice3'}
+//             onClick={() => handleChoiceClick('choice3')}
+//         >
+//             Engine volume
+//         </ChoiceButton>
+//     </ButtonContainer>
+// );
+// function ThreeChoiceButton() {
+//     return (
+//         <ButtonContainer>
+//             <ChoiceButton>
+//                 {filters.map((filter, index) => (
+//                     <StyledSelect
+//                         key={index}
+//                         value={filter}
+//                         dropdownRender={() => (
+//                             <div>
+//                                 {filters.map((option, optionIndex) => (
+//                                     <div key={optionIndex}>{option}</div>
+//                                 ))}
+//                             </div>
+//                         )}
+//                     />
+//                 ))}
+//             </ChoiceButton>
+//         </ButtonContainer>
+//     );
+// }
+// import React, { useState } from 'react';
+// import { ButtonContainer, ChoiceButton } from "./ChoiceButton.styled";
+//
+// const filters = [{
+//     title: "Price",
+//     options: ["ascending", "descending"]
+// }, {
+//     title: "Year",
+//     options: ["ascending", "descending"]
+// }, {
+//     title: "Engine volume",
+//     options: ["ascending", "descending"]
+// }];
+//
+// function ThreeChoiceButton() {
+//     const [selectedChoice, setSelectedChoice] = useState(null);
+//
+//     const handleChoiceClick = (choice) => {
+//         setSelectedChoice(selectedChoice === choice ? null : choice);
+//     };
+//
+//     return (
+//         <ButtonContainer>
+//             {filters.map((filter, index) => (
+//                 <ChoiceButton
+//                     key={index}
+//                     selected={selectedChoice === filter.title}
+//                     onClick={() => handleChoiceClick(filter.title)}
+//                 >
+//                     {filter.title}
+//                 </ChoiceButton>
+//             ))}
+//         </ButtonContainer>
+//     );
+// }
+//
+// export default ThreeChoiceButton;
 import React, {useState} from 'react';
-import {ButtonContainer, ChoiceButton} from "./ChoiceButton.styled";
+import {ButtonContainer, ChoiceButton, StyledSelect} from "./ChoiceButton.styled";
+
+const filters = [
+    {
+        title: "Price",
+        options: ["ascending", "descending"]
+    },
+    {
+        title: "Year",
+        options: ["ascending", "descending"]
+    },
+    {
+        title: "Engine volume",
+        options: ["ascending", "descending"]
+    }
+];
 
 function ThreeChoiceButton() {
-    const [selectedChoice, setSelectedChoice] = useState(null);
+    const [openFilter, setOpenFilter] = useState(null);
+    const [selectedOption, setSelectedOption] = useState(null);
 
-    const handleChoiceClick = (choice) => {
-        setSelectedChoice(selectedChoice === choice ? null : choice);
+    const toggleFilter = (filterTitle) => {
+        setOpenFilter(openFilter === filterTitle ? null : filterTitle);
+    };
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        setOpenFilter(null);
     };
 
     return (
         <ButtonContainer>
-            <ChoiceButton
-                selected={selectedChoice === 'choice1'}
-                onClick={() => handleChoiceClick('choice1')}
-            >
-                Price
-            </ChoiceButton>
-
-            <ChoiceButton
-                selected={selectedChoice === 'choice2'}
-                onClick={() => handleChoiceClick('choice2')}
-            >
-                Year
-            </ChoiceButton>
-
-            <ChoiceButton
-                selected={selectedChoice === 'choice3'}
-                onClick={() => handleChoiceClick('choice3')}
-            >
-                Engine volume
-            </ChoiceButton>
+            {filters.map((filter, index) => (
+                <div key={index}>
+                    <ChoiceButton
+                        onClick={() => toggleFilter(filter.title)}
+                        selected={openFilter === filter.title}
+                    >
+                        {filter.title}
+                    </ChoiceButton>
+                    {openFilter === filter.title && (
+                        <StyledSelect>
+                            {filter.options.map((option, optionIndex) => (
+                                <option
+                                    key={optionIndex}
+                                    value={option}
+                                    onClick={() => handleOptionClick(option)}
+                                >
+                                    {option}
+                                </option>
+                            ))}
+                        </StyledSelect>
+                    )}
+                </div>
+            ),)}
+            {selectedOption && <p>Selected: {selectedOption}</p>}
         </ButtonContainer>
     );
 }
 
 export default ThreeChoiceButton;
+
+
--- floodservice/src/component/ChoiceButton/ChoiceButton.styled.js
+++ floodservice/src/component/ChoiceButton/ChoiceButton.styled.js
@@ -5,15 +5,22 @@ export const ButtonContainer = styled.div`
   justify-content: space-between;
 `;
 
-export const ChoiceButton = styled.button`
+export const ChoiceButton = styled.div`
   padding: 10px 20px;
   border: 1px solid #ccc;
   cursor: pointer;
   transition: background-color 0.3s;
   background-color: ${(props) => (props.selected ? '#007bff' : 'transparent')};
   color: ${(props) => (props.selected ? '#fff' : 'inherit')};
+  display: flex;
+  flex-direction: column;
+    justify-content: center;
 
   &:hover {
     background-color: #f0f0f0;
   }
 `;
+
+export const StyledSelect = styled.select`
+  width: 150px;
+`
